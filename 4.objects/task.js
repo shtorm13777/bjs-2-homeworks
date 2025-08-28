d0bd5d864a98276fc1823116a921aa7a4cab316f
shtorm <shtorm777@list.ru>
2025-08-28 16:06:00 +0300

    "Объекты" задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,50 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.subject = ''; // Предмет студента
+    this.marks = [];   // Массив оценок студента
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  // Метод для установки предмета студента
+  Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+  };
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  // Метод для добавления нескольких оценок студенту
+  Student.prototype.addMarks = function(...marksToAdd) {
+    if (this.marks) {
+      this.marks.push(...marksToAdd);
+    } else {
+      console.error('Student is excluded');
+    }
+  };
   
-}
-
-Student.prototype.getAverage = function () {
+  // Метод для получения среднего балла студента
+  Student.prototype.getAverage = function() {
+    if (this.marks && this.marks.length > 0) {
+      return this.marks.reduce((sum, value) => sum + value, 0) / this.marks.length;
+    } else {
+      return 0;
+    }
+  };
   
-}
-
-Student.prototype.exclude = function (reason) {
+  // Метод для исключения студента из учебного процесса
+  Student.prototype.exclude = function(reason) {
+    this.subject = undefined;
+    this.marks = undefined;
+    this.excluded = reason;
+  };
   
-}
+  // Пример использования
+  let student1 = new Student("Василиса", "женский", 19);
+  student1.setSubject("Algebra");
+  console.log(student1.getAverage()); // 0
+  student1.addMarks(4, 5, 4, 5);
+  console.log(student1.getAverage()); // 4.5
+  console.log(student1);
+  
+  let student2 = new Student("Артём", "мужской", 25);
+  student2.setSubject("Geometry");
+  student2.exclude('плохая учёба');
+  console.log(student2);
